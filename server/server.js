8ef45ae11cbb3f76e59a50cff164ee7be577209b
Stefan1400 <stefan.besonen783@gmail.com>
2025-08-26 11:38:26 -0700

    added _redirects in public file on client (trying to fix manual routing

--- server/server.js
+++ server/server.js
@@ -14,10 +14,5 @@ app.use('/api/words', wordRoutes);
 app.use('/api/stats', statsRoutes);
 app.use('/api/review', reviewRoutes);
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Backend server is running!' });
-});
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
