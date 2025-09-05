b8c769c18d195400a2374728b4aa39c81eaecd51
Stefan1400 <stefan.besonen783@gmail.com>
2025-09-05 11:32:23 -0700

    confirm popup for both logout and delete account now work

--- client/src/components/ConfirmPopup/ConfirmPopup.jsx
+++ client/src/components/ConfirmPopup/ConfirmPopup.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import './ConfirmPopup.css';
+import { useState, useContext } from 'react';
 
-function ConfirmPopup({ confirmType, cancelDelete, confirmDelete }) {
-  return (
-    <div className='confirm-popup'>
+function ConfirmPopup({ 
+   confirmType, 
+   cancel, 
+   loggedPW, 
+   setLoggedPW, 
+   confirm 
+}) {
+
+   // STATES
+
+   // CONTEXTS
+   
+   // FUNCTIONS
+
+   return (
+    <div className={`confirm-popup ${confirmType}`}>
       <h3 className='confirm-popup-message'>{confirmType === 'delete' ? 'Delete Account?' : confirmType === 'logout' ? 'Logout?' : ''}</h3>
       {confirmType === 'delete' && (
-         <span className='confirm-popup-message-details'>All data will be permanantly deleted</span>
+         <>
+            <span className='confirm-popup-message-details'>Please enter your password to delete</span>
+
+            <input onChange={(e) => setLoggedPW(e.target.value)} value={loggedPW} type="text" className="confirm-popup-pw-confirm-input" placeholder='password' />
+
+            <span className='confirm-popup-message-details little'>Note: All data will be permanantly deleted and cannot be undone</span>
+         </>
       )}
       
       <div className="confirm-popup-btns">
-         <button onClick={cancelDelete} className="confirm-popup-btn-cancel">Cancel</button>
-         <button onClick={confirmDelete} className="confirm-popup-btn-confirm">{confirmType === 'delete' ? 'Delete' : confirmType === 'logout' ? 'Logout' : ''}</button>
+         <button onClick={cancel} className="confirm-popup-btn-cancel">Cancel</button>
+         <button onClick={confirm} className="confirm-popup-btn-confirm">{confirmType === 'delete' ? 'Delete' : confirmType === 'logout' ? 'Logout' : ''}</button>
       </div>
     </div>
   )
--- client/src/pages/AccountPage/AccountPage.jsx
+++ client/src/pages/AccountPage/AccountPage.jsx
@@ -7,7 +7,6 @@ import { XIcon } from '../../assets/Icons/Icons';
 import ConfirmPopup from '../../components/ConfirmPopup/ConfirmPopup';
 
 function AccountPage({ goToLogin, goToRegister }) {
-   
 
    const { isLoggedIn, user, logout, deleteAccount } = useContext(AuthContext);
    const { showNotification } = useContext(NotificationContext);
@@ -15,9 +14,10 @@ function AccountPage({ goToLogin, goToRegister }) {
    //logout 
       const handleLogout = async () => {
          const loggedOut = await logout();
-   
+         
          if (loggedOut) {
             showNotification('successfully logged out');
+            setShowLogoutConfirm(false);
          } else {
             showNotification('error logging out');
          }
@@ -25,11 +25,18 @@ function AccountPage({ goToLogin, goToRegister }) {
    
       //delete account
       const [isDeleting, setIsDeleting] = useState(false);
-      const [confirmOpen, setConfirmOpen] = useState(false);
+      const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+      const [showLogoutConfirm, setShowLogoutConfirm] = useState(false);
       const [loggedPW, setLoggedPW] = useState('');
    
-      const showConfirmTab = () => {
-         setConfirmOpen(true);
+      const showConfirmPopup = (type) => {
+         if (type === 'delete') {
+            setShowDeleteConfirm(true);
+            return;
+         } else if (type === 'logout') {
+            setShowLogoutConfirm(true);
+            return;
+         }
       }
    
       const handleDeleteAccount = async () => {
@@ -42,7 +49,7 @@ function AccountPage({ goToLogin, goToRegister }) {
          const deletedAccount = await deleteAccount(loggedPW);
    
          if (deletedAccount) {
-            setConfirmOpen(false);
+            setShowDeleteConfirm(false);
             showNotification('account successfully deleted');
             setIsDeleting(false);
             setLoggedPW('');
@@ -50,15 +57,32 @@ function AccountPage({ goToLogin, goToRegister }) {
       }
 
       const cancelDeleteAccount = () => {
-         setConfirmOpen(false);
+         setShowDeleteConfirm(false);
          setIsDeleting(false);
          setLoggedPW('');
       }
 
+      const cancelLogout = () => {
+         setShowLogoutConfirm(false);
+      }
+
    return (
     <div className='account-page page'>
-      {confirmOpen && (
-         <ConfirmPopup confirmType='delete' cancelDelete={cancelDeleteAccount} confirmDelete={handleDeleteAccount} />
+      {showDeleteConfirm && (
+         <ConfirmPopup 
+            confirmType='delete' 
+            cancel={cancelDeleteAccount} 
+            loggedPW={loggedPW} 
+            setLoggedPW={setLoggedPW}
+            confirm={handleDeleteAccount}
+         />
+      )}
+      {showLogoutConfirm && (
+         <ConfirmPopup 
+            confirmType='logout' 
+            cancel={cancelLogout} 
+            confirm={handleLogout}
+         />
       )}
       
       <h3 className='page-header'>Account</h3>
@@ -66,8 +90,8 @@ function AccountPage({ goToLogin, goToRegister }) {
       <h2 className='account-page-username-h2'>{`Hello, ${isLoggedIn ? user : 'Guest'}`}</h2>
       {isLoggedIn && (
             <div className="account-page-auth-btns">
-               <button onClick={handleLogout} className='auth-btn logout-btn'>Logout</button>
-               <button onClick={showConfirmTab} className='auth-btn delete-account-btn'>Delete Account</button>
+               <button onClick={() => showConfirmPopup('logout')} className='auth-btn logout-btn'>Logout</button>
+               <button onClick={() => showConfirmPopup('delete')} className='auth-btn delete-account-btn'>Delete Account</button>
             </div>
          )}
          {!isLoggedIn && (
